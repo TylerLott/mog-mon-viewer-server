422780821d0e9d9dd38cf141e853b0984b9f0434
TylerLott <tylerlott0126@gmail.com>
2022-04-12 17:49:30 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+
+const PORT = 4500
+
+const app = express()
+
+// This server should do Oauth for google
+app.use((req, res, next) => {
+  // validate
+  next()
+})
+
+// This server should also read data from Redis and send to users
+app.get("/currentStats", (req, res) => {
+  //
+})
+
+// This server should recieve team-updates from the main ws-server and update the Redis server with teams
+app.post("/mainwebsocket", (req, res) => {
+  //
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
